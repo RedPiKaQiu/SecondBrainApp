8f98f9e9937f8f2af2ef59140d3884a75b490d0e
RedPiKaQiu <zxm1994929@sina.com>
2024-11-30 09:50:27 +0800

    Initial commit

---
+++ SecondBrainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondBrainAppApp.swift
+//  SecondBrainApp
+//
+//  Created by pika on 2024/11/30.
+//
+
+import SwiftUI
+
+@main
+struct SecondBrainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
